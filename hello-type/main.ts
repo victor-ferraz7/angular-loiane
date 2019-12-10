01c73978921b9b5b648409ab005621eac2410594
Victor Ferraz <victor.ferraz@modalgr.com.br>
2019-12-09 21:11:37 -0300

    Aula #04 Introdução ao TypeScript para Angular

---
+++ hello-type/main.ts
@@ -0,0 +1,21 @@
+var minhaVar = 'minha Variável 1';
+
+function minhaFunc(x,y) {
+    return x+y;
+}
+
+let num : Number = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+    return valor*2;
+});
+
+numeros.map( valor => valor * 2)
+
+class Matematica {
+    soma (x,y) {
+        return x + y;
+    }
+}
