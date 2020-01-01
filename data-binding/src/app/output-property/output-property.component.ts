c7c55658a1e7c008eb997b9e3f7ec8c446c76f5d
Victor Ferraz <victor.ferraz@modalgr.com.br>
2019-12-31 23:52:00 -0300

    Aula #16: Acesso ao DOM e ao Template com ViewChild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,EventEmitter,Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -6,24 +6,25 @@ import { Component, OnInit, Input,EventEmitter,Output } from '@angular/core';
   styleUrls: ['./output-property.component.css']
 })
 export class OutputPropertyComponent implements OnInit {
- 
+
   @Input() valor: number = 0;
   @Output() mudouValor = new EventEmitter();
 
-  
+  @ViewChild('campoInput') campoValorInput: ElementRef;
+
   incrementa() {
-    this.valor++;
-    this.mudouValor.emit({novoValor: this.valor})
+    this.campoValorInput.nativeElement.value++;
+    this.mudouValor.emit({ novoValor: this.valor })
   }
-  
+
   decrementa() {
-    this.valor--;
-    this.mudouValor.emit({novoValor: this.valor})
+    this.campoValorInput.nativeElement.value--;
+    this.mudouValor.emit({ novoValor: this.valor })
   }
 
 
   constructor() { }
-  
+
   ngOnInit() {
   }
 }
